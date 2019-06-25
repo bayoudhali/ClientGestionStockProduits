e01edb464bca2efc133df275cf605d068f9553f4
unknown <bayoudhali44@gmail.com>
2019-06-25 16:02:15 +0200

    ajouter le module de routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {ProduitComponent} from './produit/produit.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+const routes: Routes = [
+  {path: 'produit', component: ProduitComponent},
+  {path: 'dashboard', component: DashboardComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{enableTracing: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
